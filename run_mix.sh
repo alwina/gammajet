42def4f39a2a2b7fde5c494ef2a07e3276191c54
Fernando Torales - Acosta <fernando@cori08.nersc.gov>
2021-11-03 14:30:58 -0700

    testing parallel mixing

--- run_mix.sh
+++ run_mix.sh
@@ -4,4 +4,4 @@ nmix=$2
 mix_start=$((i*nmix))
 mix_end=$((mix_start+nmix))
 echo $mix_start $mix_end
-./cpp/build/h5_mixed_event config-pbpb18.yaml $mix_start $mix_end
+./cpp/build/h5_mixed_event_parallel config-pbpb18.yaml $mix_start $mix_end
