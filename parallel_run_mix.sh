9a6fe440bbf425825d647af70c39b3657389a22d
alwina <alwina@berkeley.edu>
2021-11-18 12:56:37 -0800

    make sure we start at imix=0

--- parallel_run_mix.sh
+++ parallel_run_mix.sh
@@ -2,4 +2,4 @@
 nmix=$1
 config=$2
 mixlabel=$3
-seq 0 64 | parallel --ungroup ./run_mix.sh {%} $nmix $config $mixlabel
+seq 0 63 | parallel --ungroup ./run_mix.sh {%}-1 $nmix $config $mixlabel
--- run_mix.sh
+++ run_mix.sh
@@ -5,6 +5,4 @@ config=$3
 mixlabel=$4
 mix_start=$((i*nmix))
 mix_end=$((mix_start+nmix))
-echo Starting $config $mixlabel $mix_start $mix_end
 ./cpp/build/h5_mixed_event_parallel $config $mixlabel $mix_start $mix_end
-echo Finished $config $mixlabel $mix_start $mix_end
